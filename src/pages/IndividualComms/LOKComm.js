7bc40f661534cb4f1411a25d27003b3c8019e891
AdityaSundar1401 <90906548+AdityaSundar1401@users.noreply.github.com>
2023-04-28 13:17:20 +0530

    Update LOKComm.js

--- src/pages/IndividualComms/LOKComm.js
+++ src/pages/IndividualComms/LOKComm.js
@@ -138,7 +138,7 @@ const DetComm = ({ match, dropDown, drop, height, name }) => {
 						<div
 							style={{
 								margin: 'auto',
-							        margin-top: '30px'
+							        marginTop: '30px'
 							}}>
 							<div
 								className='btn btn-primary '
