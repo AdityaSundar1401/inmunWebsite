0c46722ec2c839d612d2ddb3eb37de701cdfbd2d
AdityaSundar1401 <90906548+AdityaSundar1401@users.noreply.github.com>
2023-04-29 13:27:56 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -221,7 +221,7 @@ const About = ({ dropDown, drop, height, name }) => {
 						</div>
 					</div>
 				</div>
-				<div style={{ position: 'relative', top: '700px' }}>
+				<div style={{ position: 'relative', top: '1000px' }}>
 					<Footer />
 				</div>
 			</div>
