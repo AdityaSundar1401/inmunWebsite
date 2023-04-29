51891259caca2eb6ceac4cded38a07173fa171b1
AdityaSundar1401 <90906548+AdityaSundar1401@users.noreply.github.com>
2023-04-29 16:22:33 +0530

    Update EB.js

--- src/pages/Secretariat/EB.js
+++ src/pages/Secretariat/EB.js
@@ -414,7 +414,7 @@ const Sec = ({ dropDown, drop, height, name }) => {
 								<div
 									className={`grid-${comm.directors.length} all-center directorBox`}>
 									{comm.directors.map((director, idx) => (
-										<div key={idx}>
+										<div key={idx} style={{position: 'relative', top: '0px'}}>
 											<img
 												src={director.image}
 												className='directorImage'
