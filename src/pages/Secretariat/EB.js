bb2f2a2d233e2d99fc08c1d350371c69aee8b609
AdityaSundar1401 <90906548+AdityaSundar1401@users.noreply.github.com>
2023-08-24 16:05:06 +0530

    Update EB.js

--- src/pages/Secretariat/EB.js
+++ src/pages/Secretariat/EB.js
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from '../../layout/Navbar';
 import Footer from '../../layout/Footer';
-import akshay from './Images/akshay3.png';
+import akshay from './Images/akshay4.png';
 import abhi from './Images/abhi2.jpg';
 import shivam from './Images/shivam.jpeg';
 import omu from '../../om.jpeg';
